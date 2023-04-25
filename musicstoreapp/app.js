58b53107e7912c3ecdb93a9bcce3c1e1685470f6
Raúl López Calvo <UO263918@uniovi.es>
2023-04-25 07:47:19 +0200

    SDI-2223-206-8.1-Acceso a datos con Mongodb desde Node.js.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -15,7 +15,7 @@ let usersRouter = require('./routes/users');
 const { MongoClient } = require("mongodb");
 const url = 'mongodb+srv://admin:sdi@eii-sdi-cluster.7uvnfo7.mongodb.net/?retryWrites=true&w=majority';
 app.set('connectionStrings', url);
-require("./routes/songs.js")(app);
+require("./routes/songs.js")(app, MongoClient);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
